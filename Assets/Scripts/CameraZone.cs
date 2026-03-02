00eb09af1951d0fd13b0f635264b2314698fcbb7
Trầm Tính Ân <taothichd3s1gn3r@gmail.com>
2026-03-02 22:44:18 +0700

    Map Module Complete
    map module top down dùng để chạy demo

---
+++ Assets/Scripts/CameraZone.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using Unity.Cinemachine; // Lưu ý: Unity 6 dùng namespace mới này
+
+public class CameraZone : MonoBehaviour
+{
+    // Kéo CM vcam1 vào đây
+    public CinemachineConfiner2D confiner; 
+    private Collider2D zoneCollider;
+
+    void Start()
+    {
+        // Lấy chính cái Collider (khung xanh) của đối tượng này
+        zoneCollider = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Nếu Player đi vào vùng này
+        if (collision.CompareTag("Player"))
+        {
+            // Đổi khung giới hạn của camera sang vùng này
+            confiner.BoundingShape2D = zoneCollider;
+            // Xóa bộ nhớ đệm để camera cập nhật ngay lập tức
+            confiner.InvalidateBoundingShapeCache();
+        }
+    }
+}
