869a982357fa5b70417b19042b4733605953f430
ProCH2405 <tranducchuan2006@gmail.com>
2026-03-10 17:21:02 +0700

    Hoan thanh cutscene cua chapter1_PhuTran

---
+++ Assets/_Code/QuanLyBaoTin.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuanLyBaoTin : MonoBehaviour
+{
+    public PlayerMovement playerScript; // Kéo Quốc Tuấn vào đây
+    public GameObject khungThoai;       // Kéo cái Bong bóng thoại vào đây
+
+    void Start()
+    {
+        // Bắt đầu vở kịch ngay khi game chạy (hoặc anh có thể gọi hàm này sau)
+        StartCoroutine(KichBanBaoTin());
+    }
+
+    IEnumerator KichBanBaoTin()
+    {
+        // 1. KHÓA CHÂN: Gọi cái công tắc canMove bên PlayerMovement
+        if (playerScript != null) playerScript.canMove = false;
+
+        // 2. CHỜ GIA NÔ CHẠY RA: Đợi khoảng 1 giây cho Gia nô chạy tới nơi
+        yield return new WaitForSeconds(1.0f);
+
+        // 3. HIỆN CHỮ: Bật khung thoại lên để bắt đầu "Lão gia đang nguy kịch..."
+        if (khungThoai != null) khungThoai.SetActive(true);
+    }
+}
